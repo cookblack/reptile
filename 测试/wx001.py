24a77779d8b774374d6cff19b86c18e80feb1a9a
lzq <284235045@qq.com>
2017-12-21 17:13:12 +0800

    测试wx取图片

--- 测试/wx001.py
+++ 测试/wx001.py
@@ -6,37 +6,42 @@ class BeautifulPicture():
 
     def __init__(self):  #类的初始化操作
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'}  #给请求指定一个请求头来模拟chrome浏览器
-        self.web_url = 'https://unsplash.com'  #要访问的网页地址
-        self.folder_path = 'D:\BeautifulPicture'  #设置图片要存放的文件目录
+        self.web_url = 'https://mp.weixin.qq.com/s/vHq09ExVR_XMSR3l8z9YcA'  #要访问的网页地址
+        self.folder_path = 'D:\BeautifulPicture\wx'  #设置图片要存放的文件目录
 
     def get_pic(self):
         print('开始网页get请求')
         r = self.request(self.web_url)
-        print('开始获取所有a标签')
-        all_a = BeautifulSoup(r.text, 'lxml').find_all('a', class_='cV68d')  #获取网页中的class为cV68d的所有a标签
+        print('开始获取所有img标签')
+        all_a = BeautifulSoup(r.text, 'lxml').find_all('img') #获取网页中的class为cV68d的所有a标签 , '[data-s="300,640"]'
+        print(all_a)
         print('开始创建文件夹')
         self.mkdir(self.folder_path)  #创建文件夹
         print('开始切换文件夹')
         os.chdir(self.folder_path)   #切换路径至上面创建的文件夹
-
+        i=1
         for a in all_a: #循环每个标签，获取标签中图片的url并且进行网络请求，最后保存图片
-            img_str = a['style'] #a标签中完整的style字符串
-            print('a标签的style内容是：', img_str)
-            first_pos = img_str.index('"') + 1
-            second_pos = img_str.index('"',first_pos)
-            img_url = img_str[first_pos: second_pos] #使用Python的切片功能截取双引号之间的内容
-            #获取高度和宽度的字符在字符串中的位置
-            width_pos = img_url.index('&w=')
-            height_pos = img_url.index('&q=')
-            width_height_str = img_url[width_pos : height_pos] #使用切片功能截取高度和宽度参数，后面用来将该参数替换掉
-            print('高度和宽度数据字符串是：', width_height_str)
-            img_url_final = img_url.replace(width_height_str, '')  #把高度和宽度的字符串替换成空字符
-            print('截取后的图片的url是：', img_url_final)
-            #截取url中参数前面、网址后面的字符串为图片名
-            name_start_pos = img_url.index('photo')
-            name_end_pos = img_url.index('?')
-            img_name = img_url[name_start_pos : name_end_pos]
-            self.save_img(img_url_final, img_name) #调用save_img方法来保存图片
+            print(a)
+            img_str = a['data-src'] #a标签中完整的style字符串
+            print('a标签的src内容是：', img_str)
+            # first_pos = img_str.index('"') + 1
+            # second_pos = img_str.index('"',first_pos)
+            # img_url = img_str[first_pos: second_pos] #使用Python的切片功能截取双引号之间的内容
+            # #获取高度和宽度的字符在字符串中的位置
+            # width_pos = img_url.index('&w=')
+            # height_pos = img_url.index('&q=')
+            # width_height_str = img_url[width_pos : height_pos] #使用切片功能截取高度和宽度参数，后面用来将该参数替换掉
+            # print('高度和宽度数据字符串是：', width_height_str)
+            # img_url_final = img_url.replace(width_height_str, '')  #把高度和宽度的字符串替换成空字符
+            # print('截取后的图片的url是：', img_url_final)
+            # #截取url中参数前面、网址后面的字符串为图片名
+            # name_start_pos = img_url.index('photo')
+            # name_end_pos = img_url.index('?')
+            img_name = i+1
+            if img_str.strip():
+                print(img_str)
+                img_url_final=img_str
+                self.save_img(img_url_final, img_name) #调用save_img方法来保存图片
 
     def save_img(self, url, name): ##保存图片
         print('开始请求图片地址，过程会有点长...')
